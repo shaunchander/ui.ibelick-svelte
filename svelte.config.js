b22c9e0f4ce6a3e28871eee4f44c20b81390ead5
shaunchander <shaunchander@gmail.com>
2023-06-17 01:47:53 -0500

    ♻️ update output

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,9 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			precompress: true,
-			strict: true
+			strict: true,
+			pages: 'public',
+			assets: 'public'
 		})
 	}
 };
